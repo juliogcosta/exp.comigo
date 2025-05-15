10c0ea0796c0e3c7575d4ebb6f7efb5153457499
juliogcosta <juliogustavocosta@gmail.com>
2025-05-15 16:37:14 -0300

    --

---
+++ usuario/src/main/resources/db/migration/V3__create_table_papel_de_usuario.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuario.papel_de_usuario (
+    id BIGSERIAL PRIMARY KEY,
+    usuario_id BIGINT NOT NULL,
+    papel_id BIGINT NOT NULL,
+    UNIQUE (usuario_id, papel_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario.usuario (id) ON DELETE CASCADE,
+    FOREIGN KEY (papel_id) REFERENCES usuario.papel (id) ON DELETE CASCADE
+);
