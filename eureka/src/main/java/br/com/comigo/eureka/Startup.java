acc7be14113b01552bc77792a4d12effc1b2f22e
juliogcosta <juliogustavocosta@gmail.com>
2025-05-15 20:31:41 -0300

    --

--- eureka/src/main/java/br/com/comigo/eureka/Startup.java
+++ eureka/src/main/java/br/com/comigo/eureka/Startup.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class Startup {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 }
