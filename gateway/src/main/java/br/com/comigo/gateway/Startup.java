c84edc9771d0be28fcae40b16c563ffdb2426937
juliogcosta <juliogustavocosta@gmail.com>
2025-05-15 20:05:38 -0300

    --

--- gateway/src/main/java/br/com/comigo/gateway/Startup.java
+++ gateway/src/main/java/br/com/comigo/gateway/Startup.java
@@ -1,13 +1,36 @@
 package br.com.comigo.gateway;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication()
-public class Startup {
+public class Startup implements CommandLineRunner {
+
+    @Value("${yc.api.gateway.cache.request-limit.ttl}")
+    private Integer ttl;
+
+    @Value("${yc.api.management.log.trace.enable}")
+    private Boolean lt = false;
+
+    public static Boolean monitorNotFoundError = false;
+
+    final static public Map<String, JSONArray> monitorNotFoundErrorMap = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+		
+    }
 }
